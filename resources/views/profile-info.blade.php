23b88785db95ec9427ed8fffc35d7256e10f8a14
Yan <yan.lysechko88@mail.ru>
2023-05-22 23:09:56 +0300

    fix migration

--- resources/views/profile-info.blade.php
+++ resources/views/profile-info.blade.php
@@ -2,9 +2,11 @@
 
 @section('profile-content')
     <div style="margin: 30px; font-size: 24px;">
+        @if($userData)
         <div style="margin: 10px 0px"><b>Имя: </b>{{$userData->first_name}}</div>
         <div style="margin: 10px 0px"><b>Телефон: </b>{{$userData->phone_number}}</div>
         <div style="margin: 10px 0px"><b>Дата рождения: </b>{{$userData->birth_date}}</div>
         <div style="margin: 10px 0px"><b>Категория: </b>{{\App\Models\UserInfo::SELECT[$userData->category]}}</div>
+        @endif
     </div>
 @endsection
